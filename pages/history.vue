b0338e97620b256dda34f412d49d62aae7b03840
Axor9 <angmarmos@gmail.com>
2023-06-12 21:10:08 +0200

    update history

--- pages/history.vue
+++ pages/history.vue
@@ -6,12 +6,15 @@ const web3Store = useWeb3Store()
 
 <template>
     <div class="History">
-        <ul class="History-list">
-            <li>Our hero embarks on a new adventure.</li>
-            <li v-for="text in web3Store.statesText" :key="text">
-                {{ text }}
-            </li>
-        </ul>
+        <h1 class="History-title">The hero's journey</h1>
+        <div class="History-info">
+            <ul class="History-info-list">
+                <li>Our hero embarks on a new adventure.</li>
+                <li v-for="text in web3Store.statesText" :key="text">
+                    {{ text }}
+                </li>
+            </ul>
+        </div>
     </div>
 </template>
 
@@ -21,8 +24,24 @@ const web3Store = useWeb3Store()
     display: flex;
     justify-content: center;
     align-items: center;
+    flex-direction: column;
 
-    &-list {
+    &-title {
+        padding-bottom: 20px;
+    }
+
+    &-info {
+        background-color: #8c8686;
+        border: #c6e4e2 solid 12px;
+        color: white;
+        height: 70%;
+        width: 30%;
+        overflow-y: auto;
+        overflow-x: hidden;
+
+        &-list {
+            font-size: large;
+        }
     }
 }
 </style>
